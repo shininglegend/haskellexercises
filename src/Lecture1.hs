9f78705aa41e0c8ed8ab5ef76dcd69c97d50964c
Shining Legend <jvctext@gmail.com>
2024-11-25 08:02:03 -0500

    lecture 1 done

--- src/Lecture1.hs
+++ src/Lecture1.hs
@@ -30,6 +30,7 @@ module Lecture1
     , strSum
     , lowerAndGreater
     ) where
+import GHC.Base (VecElem(Int16ElemRep))
 
 -- VVV If you need to import libraries, do it after this line ... VVV
 
@@ -39,14 +40,13 @@ module Lecture1
 its behaviour, possible types for the function arguments and write the
 type signature explicitly.
 -}
+makeSnippet :: Int -> [Char] -> [Char]
 makeSnippet limit text = take limit ("Description: " ++ text) ++ "..."
 
 {- | Implement a function that takes two numbers and finds sum of
 their squares.
-
 >>> sumOfSquares 3 4
 25
-
 >>> sumOfSquares (-2) 7
 53
 
@@ -54,7 +54,8 @@ Explanation: @sumOfSquares 3 4@ should be equal to @9 + 16@ and this
 is 25.
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-sumOfSquares x y = error "TODO!"
+sumOfSquares :: Num a => a -> a -> a
+sumOfSquares x y = (x ^ 2) + (y ^ 2)
 
 {- | Implement a function that returns the last digit of a given number.
 
@@ -67,7 +68,8 @@ sumOfSquares x y = error "TODO!"
 
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-lastDigit n = error "lastDigit: Not implemented!"
+lastDigit :: Integral a => a -> a
+lastDigit n = mod (abs n) 10
 
 {- | Write a function that takes three numbers and returns the
 difference between the biggest number and the smallest one.
@@ -81,7 +83,16 @@ and 1 is the smallest, and 7 - 1 = 6.
 Try to use local variables (either let-in or where) to implement this
 function.
 -}
-minmax x y z = error "TODO"
+minmax :: Int -> Int -> Int -> Int
+minmax x y z =
+    let 
+        findMax :: Int -> [Int] -> Int
+        findMax a [] = a 
+        findMax a (h:t) = if a > h then findMax a t else findMax h t
+        findMin :: Int -> [Int] -> Int
+        findMin a [] = a 
+        findMin a (h:t) = if a < h then findMin a t else findMin h t
+    in findMax x [y, z] - findMin x [y, z]
 
 {- | Implement a function that takes a string, start and end positions
 and returns a substring of a given string from the start position to
@@ -98,7 +109,11 @@ start position can be considered as zero (e.g. substring from the
 first character) and negative end position should result in an empty
 string.
 -}
-subString start end str = error "TODO"
+subString start end str = 
+    let 
+        actualend = max (end + 1) 0
+        actualstart = if actualend > 0 then max start 0 else 0 -- Make sure setting it to 0 gives nothing
+    in drop actualstart (take actualend str)
 
 {- | Write a function that takes a String — space separated numbers,
 and finds a sum of the numbers inside this string.
@@ -108,7 +123,13 @@ and finds a sum of the numbers inside this string.
 
 The string contains only spaces and/or numbers.
 -}
-strSum str = error "TODO"
+strSum str = 
+    let 
+        numbers = map read (words str)
+        addUp :: [Int] -> Int -> Int
+        addUp [] a = a -- Could use fold, I suppose
+        addUp (h:t) a = addUp t (h + a)
+    in addUp numbers 0
 
 {- | Write a function that takes a number and a list of numbers and
 returns a string, saying how many elements of the list are strictly
@@ -123,4 +144,14 @@ and lower than 6 elements (4, 5, 6, 7, 8 and 9).
 
 🕯 HINT: Use recursion to implement this function.
 -}
-lowerAndGreater n list = error "TODO"
+lowerAndGreater n list = 
+    let 
+        countBiggerAndSmaller :: [Int] -> Int -> (Int, Int) -> (Int, Int)
+        countBiggerAndSmaller [] _ (b, s) = (b, s)
+        countBiggerAndSmaller (h : t) n (b, s)
+            | n > h = countBiggerAndSmaller t n (b + 1, s)
+            | n < h = countBiggerAndSmaller t n (b, s + 1)
+            | otherwise = countBiggerAndSmaller t n (b, s)
+        (bigger, smaller) = countBiggerAndSmaller list n (0, 0)
+    -- https://stackoverflow.com/questions/2784271/haskell-converting-int-to-string
+    in show n ++ " is greater than " ++ show bigger ++ " elements and lower than " ++ show smaller ++ " elements"
